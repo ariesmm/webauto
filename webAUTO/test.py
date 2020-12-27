89411a9f56551f0e27ea8f09f49b56115ff474d1
ming <1402726912@qq.com>
2020-12-28 00:31:00 +0800

    123

--- webAUTO/test.py
+++ webAUTO/test.py
@@ -1,10 +1,10 @@
-from selenium import webdriver  # 引入webdriver
-import time
-
-driver = webdriver.Chrome() #启动Chrome浏览器，也是实例化Chrome
-driver.get("http://www.baidu.com") #打开百度首页
-
-time.sleep(2)
-
-driver.quit()
+# from selenium import webdriver  # 引入webdriver
+# import time
+#
+# driver = webdriver.Chrome() #启动Chrome浏览器，也是实例化Chrome
+# driver.get("http://www.baidu.com") #打开百度首页
+#
+# time.sleep(2)
+#
+# driver.quit()
 print("利用Jenkins进行新建项目，持续集成构建操作")
